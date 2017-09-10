3f66d665fc31acf9f6b99649e7b94de5baed9ec6
Deforis Nash <defhnash@gmail.com>
2017-09-10 01:40:25 -0500

    got angular working

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("hullo");
+
+var application = angular.module('application', []);
+
+application.controller('Test', ['$scope', '$http', function (scope, http){
+console.log("angular loaded");
+
+}]);
